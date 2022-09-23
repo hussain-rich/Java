efb85bee94913953cae4ab941269357748192051
Hameed Hussain <101456259+hussain-rich@users.noreply.github.com>
2022-09-23 19:57:58 +0530

    java basics

---
+++ PrintAsciiValue.java
@@ -0,0 +1,14 @@
+package javabasic;
+
+public class PrintAsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	char ch='H';
+	int ascii=ch;
+	int castAscii=(int) ch;
+	System.out.println("The ASCII value of " + ch +" is:"+ascii);
+	System.out.println("The ASCII value of " + ch +" is"+castAscii);
+	}
+
+}
