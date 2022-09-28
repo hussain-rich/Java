5b7d793c2d896ae6d8244aab1cf58363a5522fce
Hameed Hussain <101456259+hussain-rich@users.noreply.github.com>
2022-09-28 17:41:50 +0530

    Add files via upload
    basic1

---
+++ Vowel.java
@@ -0,0 +1,11 @@
+package assignment02;
+
+public class Vowel {
+public static void main(String[] args) {
+	char z10='i';
+	if(z10=='a'||z10=='e'||z10=='i'||z10=='o'||z10=='u')
+		System.out.println(z10+"...is vowel letter");
+	else
+		System.out.println(z10+"...is consonent");
+}
+}
